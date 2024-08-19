f2a9c50a1e33eba9411cc5e88869b6ddeabf2141
raze0017 <appurazeem2000@gmail.com>
2024-08-19 22:47:50 +0530

    implement navigation bar with smooth transition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './css/styles.css';
+import './css/addtional.css';
+
+const but = document.querySelector('.menu-button');
+const menu = document.querySelector('.side-menu');
+const body = document.querySelector('body');
+
+but.addEventListener('click', () => {
+  if (menu.style.left === '-250px' || menu.style.left === '') {
+    menu.style.left = '0px';
+    body.style.left = '400px';
+    but.style.left = '400px';
+  } else {
+    menu.style.left = '-250px';
+    body.style.left = '0px';
+    but.style.left = '0px';
+  }
+});
+
+document.querySelectorAll('.menu-item').forEach((item) => {
+  item.addEventListener('click', function () {
+    document
+      .querySelectorAll('.menu-item')
+      .forEach((link) => link.classList.remove('active'));
+    this.classList.add('active');
+
+    menu.style.left = '-250px';
+    but.style.left = '0px';
+    body.style.left = '0px';
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth',
+    });
+  });
+});
